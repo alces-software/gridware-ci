c33f549d6edf4c0b960c0253b354b8df4e1603d0
Mark Titorenko <mark.titorenko@alces-software.com>
2016-03-18 00:29:02 +0000

    Make PRs work.

--- build-container.sh
+++ build-container.sh
@@ -4,7 +4,11 @@ sudo service docker stop
 sudo apt-get update
 sudo apt-get -y -o Dpkg::Options::=--force-confdef -o Dpkg::Options::="--force-confnew" install docker-engine
 REPO="${TRAVIS_REPO_SLUG}"
-REPO="alces-software/packager-base"
-docker build --build-arg treeish=${TRAVIS_COMMIT} --build-arg repo_slug="${REPO}" \
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+    TREEISH="${TRAVIS_COMMIT}"
+else
+    TREEISH="$(git rev-parse HEAD^2)"
+fi
+docker build --build-arg treeish=${TREEISH} --build-arg repo_slug="${REPO}" \
        -t "alces/packages-${TRAVIS_COMMIT}-${cw_DIST}-${cw_VERSION}" \
        ".gridware-ci/${cw_DIST}-${cw_VERSION}"
