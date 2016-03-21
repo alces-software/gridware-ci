72677fa8f5c647296085d306fa9d753184562705
Mark Titorenko <mark.titorenko@alces-software.com>
2016-03-21 17:09:36 +0000

    Rename deploy script; explicitly upload logs

--- upload-artifacts.sh
+++ upload-artifacts.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+export ARTIFACTS_REGION='eu-west-1'
 if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    export ARTIFACTS_REGION='eu-west-1'
     artifacts -f multiline upload \
 	      --permissions=public-read \
 	      --target-paths 'gridware' \
 	      --working-dir "$HOME" \
 	      '$dist'
 fi
+artifacts -f multiline upload \
+	  --target-paths 'gridware_builds' \
+	  working_dir: "$HOME/logs" \
+          build-$TRAVIS_BUILD_NUMBER
