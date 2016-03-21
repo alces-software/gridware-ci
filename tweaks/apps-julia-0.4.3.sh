e5be2a0d64fde8e4bd3e7c33de6bb2eb702110c6
Mark Titorenko <mark.titorenko@alces-software.com>
2016-03-21 16:44:00 +0000

    Use bash to run curl.

--- tweaks/apps-julia-0.4.3.sh
+++ tweaks/apps-julia-0.4.3.sh
@@ -11,7 +11,7 @@ deps+=(libs-arpack-ng-3.2.0)
 deps+=(libs-libgit2-0.23.4)
 deps+=(mpi-openmpi-1.8.5)
 for dep in "${deps[@]}"; do
-    docker run ${img}:build "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
     docker commit $(docker ps -alq) $img:build
     docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
     docker commit $(docker ps -alq) $img:build
--- tweaks/libs-fftw3-3.3.4.sh
+++ tweaks/libs-fftw3-3.3.4.sh
@@ -4,7 +4,7 @@ export_packages="fftw3_float fftw3_double fftw3_long-double"
 # install dependencies
 deps=(mpi-openmpi-1.8.5)
 for dep in "${deps[@]}"; do
-    docker run ${img}:build "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
     docker commit $(docker ps -alq) $img:build
     docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
     docker commit $(docker ps -alq) $img:build
