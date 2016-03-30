53bf5d8392687c79c3a715f317371a5b671ae039
Mark Titorenko <mark.titorenko@alces-software.com>
2016-03-30 19:27:42 +0100

    Build tweaks for scipy 0.17.0

---
+++ tweaks/libs-scipy-0.17.0.sh
@@ -0,0 +1,13 @@
+install_args="--variant=all"
+export_args="--ignore-bad"
+export_packages="libs/scipy libs/scipy_python3 libs/scipy_python34"
+# install dependencies
+deps=(apps-patchelf-0.9 libs-atlas-3.10.2)
+deps+=(libs-numpy-1.9.2 libs-numpy_python3-1.9.2 libs-numpy_python34-1.9.2)
+deps+=(apps-python-2.7.8 apps-python3-3.3.3 apps-python3-3.4.3)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
