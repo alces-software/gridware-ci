0740bc32613924776623281c97bbf68402545e88
Mark Titorenko <mark.titorenko@alces-software.com>
2016-03-30 18:40:28 +0100

    add patchelf as dependency for libs/numpy

--- tweaks/libs-numpy-1.9.2.sh
+++ tweaks/libs-numpy-1.9.2.sh
@@ -2,7 +2,7 @@ install_args="--variant=all"
 export_args="--ignore-bad"
 export_packages="libs/numpy libs/numpy_python3 libs/numpy_python34"
 # install dependencies
-deps=(libs-atlas-3.10.2 apps-python-2.7.8 apps-python3-3.3.3 apps-python3-3.4.3)
+deps=(libs-atlas-3.10.2 apps-python-2.7.8 apps-python3-3.3.3 apps-python3-3.4.3 apps-patchelf-0.9)
 for dep in "${deps[@]}"; do
     docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
     docker commit $(docker ps -alq) $img:build
