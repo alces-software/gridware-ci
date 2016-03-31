849bfa08349989f1644cb067b234c067401df24c
Mark Titorenko <mark.titorenko@alces-software.com>
2016-03-31 12:35:42 +0100

    Allow bad paths for export of bsoft.

--- tweaks/apps-bsoft-1.9.1.sh
+++ tweaks/apps-bsoft-1.9.1.sh
@@ -1,4 +1,5 @@
 # install dependencies
+export_args="--ignore-bad"
 deps=(libs-fftw3_float-3.3.4 libs-tkimg-1.4.2)
 for dep in "${deps[@]}"; do
     docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
