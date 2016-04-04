9dbcd2156175bf4fba45e815105355714d05e66b
Mark Titorenko <mark.titorenko@alces-software.com>
2016-04-04 12:13:04 +0100

    Build tweaks for Theano 0.8.0

---
+++ tweaks/libs-theano-0.8.0.sh
@@ -0,0 +1,13 @@
+install_args="--variant=all"
+export_packages="libs/theano/0.8.0 libs/theano_python3/0.8.0 libs/theano_python34/0.8.0"
+# install dependencies
+deps=(libs-atlas-3.10.2)
+deps+=(apps-python-2.7.8 apps-setuptools-15.1 libs-numpy-1.9.2 libs-scipy-0.17.0)
+deps+=(apps-python3-3.3.3 apps-setuptools_python3-15.1 libs-numpy_python3-1.9.2 libs-scipy_python3-0.17.0)
+deps+=(apps-python3-3.4.3 apps-setuptools_python34-15.1 libs-numpy_python34-1.9.2 libs-scipy_python34-0.17.0)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
