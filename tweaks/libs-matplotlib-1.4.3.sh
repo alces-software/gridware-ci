934143a10a995cb52ee481e39b3b3a37404ed6ea
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-04-08 17:25:25 +0100

    Build Tweaks for setuptools v2.1 and matplotlib v1.4.3

---
+++ tweaks/libs-matplotlib-1.4.3.sh
@@ -0,0 +1,10 @@
+install_args="--variant=all"
+export_packages="libs/matplotlib/1.4.3 libs/matplotlib_python3/1.4.3 libs/matplotlib_python34/1.4.3"
+# install dependencies
+deps=(apps-python-2.7.8 apps-python3-3.3.3 apps-python3-3.4.3 apps-setuptools_python3-2.1 apps-setuptools_python34-2.1)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
