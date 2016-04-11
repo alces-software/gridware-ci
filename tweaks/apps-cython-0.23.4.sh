48bb7e7b0ac2d388080fd30b2a9fa8a16b8fc216
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-04-11 17:00:53 +0100

    Build Tweaks for Cython v0.23.4

---
+++ tweaks/apps-cython-0.23.4.sh
@@ -0,0 +1,10 @@
+install_args="--variant=all"
+export_packages="apps/cython/0.23.4 apps/cython_python3/0.23.4 apps/cython_python34/0.23.4"
+# install dependencies
+deps=(apps-python-2.7.8 apps-python3-3.3.3 apps-python3-3.4.3)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
