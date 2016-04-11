781c574a5ca9690b602a09abf042dc8865d4f4d2
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-04-11 14:56:48 +0100

    Updated Build Tweaks for matplotlib v1.4.3

--- tweaks/libs-matplotlib-1.4.3.sh
+++ tweaks/libs-matplotlib-1.4.3.sh
@@ -1,7 +1,7 @@
 install_args="--variant=all"
 export_packages="libs/matplotlib/1.4.3 libs/matplotlib_python3/1.4.3 libs/matplotlib_python34/1.4.3"
 # install dependencies
-deps=(apps-python-2.7.8 apps-python3-3.3.3 apps-python3-3.4.3 libs-numpy-1.9.2 libs-numpy_python3-1.9.2 libs-numpy-python34-1.9.2 apps-setuptools-2.1 apps-setuptools_python3-2.1 apps-setuptools_python34-2.1)
+deps=(apps-python-2.7.8 apps-python3-3.3.3 apps-python3-3.4.3 libs-atlas-3.10.2 libs-numpy-1.9.2 libs-numpy_python3-1.9.2 libs-numpy-python34-1.9.2 apps-setuptools-2.1 apps-setuptools_python3-2.1 apps-setuptools_python34-2.1)
 for dep in "${deps[@]}"; do
     docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
     docker commit $(docker ps -alq) $img:build
