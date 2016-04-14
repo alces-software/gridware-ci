3c30fbdf85145d6dc51ab3139e5917bd37a361c6
Mark Titorenko <mark.titorenko@alces-software.com>
2016-04-14 14:33:16 +0100

    Only attempt artifact upload if we have secure env vars set.

--- upload-artifacts.sh
+++ upload-artifacts.sh
@@ -1,14 +1,17 @@
-#!/bin/bash -x
+#!/bin/bash
 export ARTIFACTS_REGION='eu-west-1'
-env | grep ARTIFACTS | grep -v SECRET
-if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+    if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+        artifacts -f multiline upload \
+	          --permissions=public-read \
+	          --target-paths 'gridware' \
+	          --working-dir "$HOME" \
+	          '$dist'
+    fi
     artifacts -f multiline upload \
-	      --permissions=public-read \
-	      --target-paths 'gridware' \
-	      --working-dir "$HOME" \
-	      '$dist'
+	      --target-paths 'gridware_builds' \
+	      --working-dir "$HOME/logs" \
+              build-$TRAVIS_BUILD_NUMBER
+else
+    echo "Skipping artifact upload as we don't appear to have secure env vars set."
 fi
-artifacts -f multiline upload \
-	  --target-paths 'gridware_builds' \
-	  --working-dir "$HOME/logs" \
-          build-$TRAVIS_BUILD_NUMBER
