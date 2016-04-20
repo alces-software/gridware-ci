41c36ae1531b590138c34a98bde5dc9272a3768c
Mark Titorenko <mark.titorenko@alces-software.com>
2016-04-20 13:33:22 +0100

    Build tweaks for protobuf v2.5.0

---
+++ tweaks/libs-protobuf-2.5.0.sh
@@ -0,0 +1,9 @@
+export_args="--ignore-bad"
+# install dependencies
+deps=(apps-python-2.7.8 apps-setuptools-15.1)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
