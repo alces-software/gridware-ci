8cb504ee076f9b12d7b2d59ae993f9693297daa3
Mark Titorenko <mark.titorenko@alces-software.com>
2016-04-20 11:08:28 +0100

    Correct ordering of requirement import.

--- tweaks/apps-memesuite-4.11.1.sh
+++ tweaks/apps-memesuite-4.11.1.sh
@@ -1,7 +1,7 @@
 install_args="--variant=all"
 export_packages="apps/memesuite/4.11.1 apps/memesuite_mpi/4.11.1"
 # install dependencies
-deps=(mpi-openmpi-1.8.5 apps-cpanminus-1.5017 apps-perl-5.20.2 apps-python-2.7.8)
+deps=(mpi-openmpi-1.8.5 apps-perl-5.20.2 apps-cpanminus-1.5017 apps-python-2.7.8)
 for dep in "${deps[@]}"; do
     docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
     docker commit $(docker ps -alq) $img:build
@@ -9,6 +9,5 @@ for dep in "${deps[@]}"; do
     docker commit $(docker ps -alq) $img:build
 done
 # hack perl module to mitigate alces-software/packager-base#77
-. /opt/clusterware/etc/distro.rc
 docker run ${img}:build /bin/bash -l -c "echo -e 'append-path PERL5LIB ${appdir}/lib/site_perl/5.20.2/x86_64-linux\nappend-path PERL5LIB ${appdir}/lib/site_perl/5.20.2\nappend-path PERL5LIB ${appdir}/lib/5.20.2/x86_64-linux\nappend-path PERL5LIB ${appdir}/lib/5.20.2' >> /opt/gridware/local/${cw_DIST}/etc/modules/apps/perl/5.20.2/*"
 docker commit $(docker ps -alq) $img:build
