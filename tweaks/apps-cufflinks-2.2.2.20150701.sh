db177a231e23e3c46adefef874d737f75952ae4f
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-05-03 17:58:07 +0100

    Build Tweaks for Cufflinks v2.2.2.20150701

---
+++ tweaks/apps-cufflinks-2.2.2.20150701.sh
@@ -0,0 +1,8 @@
+# install dependencies
+deps=(mpi-openmpi-1.8.5 apps-python-2.7.8 libs-boost-1.55.0 apps-samtools-0.1.19 libs-eigen-3.2.4)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
