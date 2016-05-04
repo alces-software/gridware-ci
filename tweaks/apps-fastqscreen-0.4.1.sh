ee0420cf301a9aaac4da59c6a87bc6107c12a2b8
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-05-04 17:13:19 +0100

    Updated Build Tweaks for fastqscreen v0.4.1

--- tweaks/apps-fastqscreen-0.4.1.sh
+++ tweaks/apps-fastqscreen-0.4.1.sh
@@ -1,6 +1,6 @@
 #export_args="--ignore-bad"
 # install dependencies
-deps=(apps-perl-5.20.2 apps-cpanminus-1.5017 apps-bowtie-2.2.6)
+deps=(apps-perl-5.20.2 apps-cpanminus-1.5017 apps-bowtie2-2.2.6)
 for dep in "${deps[@]}"; do
     docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
     docker commit $(docker ps -alq) $img:build
