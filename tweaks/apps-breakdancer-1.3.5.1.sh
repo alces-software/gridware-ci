90e3bf22374d89761b122efbf98a7dede5ce4c52
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-05-05 12:07:59 +0100

    Build Tweaks for Breakdancer v1.3.5.1

--- tweaks/apps-breakdancer-1.3.5.1.sh
+++ tweaks/apps-breakdancer-1.3.5.1.sh
@@ -1,6 +1,6 @@
 # export_args="--ignore-bad"
 # install dependencies
-deps=(apps-cmake-3.5.2 libs-boost-1.60.0 apps-samtools-0.1.18)
+deps=(apps-cmake-3.5.2 mpi-openmpi-1.8.5 apps-python-2.7.8 libs-boost-1.60.0 apps-samtools-0.1.18)
 for dep in "${deps[@]}"; do
     docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
     docker commit $(docker ps -alq) $img:build
