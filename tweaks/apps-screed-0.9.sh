a33ea2428826ea625bb815a9ddf995b05daca814
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-05-09 17:44:21 +0100

    Build Tweaks for Screed v0.9

---
+++ tweaks/apps-screed-0.9.sh
@@ -0,0 +1,11 @@
+install_args="--variant=all"
+# export_args="--ignore-bad"
+export_packages="apps/screed/0.9 apps/screed_python3/0.9 apps/screed_python34/0.9"
+# install dependencies
+deps=(apps-python-2.7.8 apps-python3-3.3.3 apps-python3-3.4.3 apps-setuptools-15.1 apps-setuptools_python3-15.1 apps-setuptools_python34-15.1)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
