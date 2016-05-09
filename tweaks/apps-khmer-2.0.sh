f4c78d854af3386304978fbc9112ac177ee17cce
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-05-09 18:28:56 +0100

    Updated Build Tweaks for Khmer v2.0

--- tweaks/apps-khmer-2.0.sh
+++ tweaks/apps-khmer-2.0.sh
@@ -1,5 +1,5 @@
 install_args="--variant=all"
-# export_args="--ignore-bad"
+export_args="--ignore-bad"
 export_packages="apps/khmer/2.0 apps/khmer_python3/2.0 apps/khmer_python34/2.0"
 # install dependencies
 deps=(apps-python-2.7.8 apps-python3-3.3.3 apps-python3-3.4.3 apps-setuptools-15.1 apps-setuptools_python3-15.1 apps-setuptools_python34-15.1 apps-screed-0.9 apps-screed_python3-0.9 apps-screed_python34-0.9)
