b14a4f108452d4898175a039984c9558171383d7
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-05-12 17:22:56 +0100

    Build Tweaks for cortexvar v1.0.5.14

---
+++ tweaks/apps-cortexvar-1.0.5.14.sh
@@ -0,0 +1,9 @@
+# export_args="--ignore-bad"
+# install dependencies
+deps=(libs-gsl-1.16 apps-perl-5.20.2 apps-vcftools-0.1.12b apps-stampy-1.0.22.1848 apps-samtools-0.1.19)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
