42e3f3d8939ceef2b43d3f746fd5d57c2eeb7f99
Mark Titorenko <mark.titorenko@alces-software.com>
2016-05-27 18:33:29 +0100

    Fix python3 numpy smoke tests.

--- tests/libs-numpy-1.9.2-python34.sh
+++ tests/libs-numpy-1.9.2-python34.sh
@@ -5,12 +5,12 @@ import timeit
 
 try:
     import numpy.core._dotblas
-    print 'FAST BLAS'
+    print('FAST BLAS')
 except ImportError:
-    print 'slow blas'
+    print('slow blas')
 
-print "version:", numpy.__version__
-print "maxint:", sys.maxint
+print("version:", numpy.__version__)
+print("maxint:", sys.maxint)
 print
 
 x = numpy.random.random((1000,1000))
@@ -19,5 +19,5 @@ setup = "import numpy; x = numpy.random.random((1000,1000))"
 count = 5
 
 t = timeit.Timer("numpy.dot(x, x.T)", setup=setup)
-print "dot:", t.timeit(count)/count, "sec"
+print("dot:", t.timeit(count)/count, "sec")
 EOF
