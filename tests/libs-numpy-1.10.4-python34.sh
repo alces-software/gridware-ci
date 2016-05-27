1f54d5eda9b59db57d05e39678ac72a8552bd523
Mark Titorenko <mark.titorenko@alces-software.com>
2016-05-27 18:34:08 +0100

    Fix python3 numpy smoke tests.

--- tests/libs-numpy-1.10.4-python34.sh
+++ tests/libs-numpy-1.10.4-python34.sh
@@ -3,8 +3,8 @@ import numpy
 import sys
 import timeit
 
-print "version:", numpy.__version__
-print "maxint:", sys.maxint
+print("version:", numpy.__version__)
+print("maxint:", sys.maxint)
 print
 
 x = numpy.random.random((1000,1000))
@@ -13,5 +13,5 @@ setup = "import numpy; x = numpy.random.random((1000,1000))"
 count = 5
 
 t = timeit.Timer("numpy.dot(x, x.T)", setup=setup)
-print "dot:", t.timeit(count)/count, "sec"
+print("dot:", t.timeit(count)/count, "sec")
 EOF
