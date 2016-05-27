4ddc70cd87bd1f2097ea32818e4e34cfea875fb6
Mark Titorenko <mark.titorenko@alces-software.com>
2016-05-27 15:13:29 +0100

    smoke tests for numpy

---
+++ tests/libs-numpy-1.10.4.sh
@@ -0,0 +1,17 @@
+python <<EOF
+import numpy
+import sys
+import timeit
+
+print "version:", numpy.__version__
+print "maxint:", sys.maxint
+print
+
+x = numpy.random.random((1000,1000))
+
+setup = "import numpy; x = numpy.random.random((1000,1000))"
+count = 5
+
+t = timeit.Timer("numpy.dot(x, x.T)", setup=setup)
+print "dot:", t.timeit(count)/count, "sec"
+EOF
