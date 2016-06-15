b1d1d5ff4f2e8a0bb0114465551eafa04072787c
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-06-15 17:23:54 +0100

    Build Tweaks for Octopus v5.0.1

---
+++ tweaks/apps-octopus-5.0.1.sh
@@ -0,0 +1,8 @@
+install_args="withcheck=true"
+deps=(libs-fftw3-3.3.4)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
