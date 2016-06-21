3749703bf5c01d38101b7871501e20d14e20cecd
Mark Titorenko <mark.titorenko@alces-software.com>
2016-06-21 16:11:54 +0100

    Reinstate memesuite mpi

--- tweaks/apps-memesuite-4.11.1.sh
+++ tweaks/apps-memesuite-4.11.1.sh
@@ -1,5 +1,2 @@
-# mpi variant disabled from CI for now due to alces-software/packager-base#80
-#install_args="--variant=all"
-#export_packages="apps/memesuite/4.11.1 apps/memesuite_mpi/4.11.1"
-install_args="--variant=default"
-export_packages="apps/memesuite/4.11.1"
+install_args="--variant=all"
+export_packages="apps/memesuite/4.11.1 apps/memesuite_mpi/4.11.1"
