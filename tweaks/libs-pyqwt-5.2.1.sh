9c76ad1ea3c9808b6a5edc0812b04cc9437159de
Mark Titorenko <mark.titorenko@alces-software.com>
2016-07-06 11:30:06 +0100

    Updated tweaks for pyqwt.

--- tweaks/libs-pyqwt-5.2.1.sh
+++ tweaks/libs-pyqwt-5.2.1.sh
@@ -1,4 +1,6 @@
 # install dependencies
+install_args="--variant all"
+export_packages="libs/pyqwt_qt46/5.2.1 libs/pyqwt_qt48/5.2.1"
 deps=(libs-atlas-3.10.2)
 for dep in "${deps[@]}"; do
     docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
