0a108faa00158c7bdda5b907647abb4b094fa569
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-07-08 14:17:05 +0100

    Build Tweaks for Bioconductor v2.14

---
+++ tweaks/libs-bioconductor-2.14.sh
@@ -0,0 +1,9 @@
+export_args="--ignore-bad"
+# install dependencies
+deps=(libs-lapack-3.5.0 libs-blas-3.6.0)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
