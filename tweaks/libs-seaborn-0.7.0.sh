f029c130bbe738aa8656021815f9d83a04feb58a
Mark Titorenko <mark.titorenko@alces-software.com>
2016-07-12 11:42:34 +0100

    Tweak for seaborn

---
+++ tweaks/libs-seaborn-0.7.0.sh
@@ -0,0 +1,10 @@
+install_args="--variant=all"
+export_packages="libs/seaborn/0.7.0 libs/seaborn_python3/0.7.0 libs/seaborn_python34/0.7.0"
+# install dependencies
+deps=(libs-atlas-3.10.2)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
