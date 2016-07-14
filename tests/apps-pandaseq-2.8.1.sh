5cbb9ce9edc0665a2f8787f8321383a57a3c0842
Mark Titorenko <mark.titorenko@alces-software.com>
2016-07-14 10:36:43 +0100

    Disable wget-based test for pandaseq for now

--- tests/apps-pandaseq-2.8.1.sh
+++ tests/apps-pandaseq-2.8.1.sh
@@ -1,3 +1,4 @@
-wget http://neufeldserver.uwaterloo.ca/~apmasell/pandaseq_sampledata.tar
-tar -xvf pandaseq_sampledata.tar
-pandaseq -f mcbath_1.fastq.bz2 -r mcbath_2.fastq.bz2 2&> pandaseq.out
+#wget http://neufeldserver.uwaterloo.ca/~apmasell/pandaseq_sampledata.tar
+#tar -xvf pandaseq_sampledata.tar
+#pandaseq -f mcbath_1.fastq.bz2 -r mcbath_2.fastq.bz2 2&> pandaseq.out
+pandaseq -h 2>&1 | grep "^pandaseq 2.8"
