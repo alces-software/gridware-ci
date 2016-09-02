0319dc4e5bae2f36a6a3665ece084b2ec479ada0
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-09-02 12:58:55 +0100

    Build Tweaks for FastLowess v20130915

---
+++ tweaks/apps-fastlowess-20130915.sh
@@ -0,0 +1,8 @@
+# install dependencies
+deps=(apps-python-2.7.8 apps-cython-0.23.4 libs-atlas-3.10.2 libs-numpy-1.9.2 apps-setuptools-15.1)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
