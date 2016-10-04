8cad1ed68be26848b0a4a9330bbca1ed6b6dd063
Andrew Provis <andrew.provis@alces-software.com>
2016-10-04 12:43:59 +0100

    Smoke Test for PLINK v1.90.150618

--- tests/apps-plink-1.90.150618.sh
+++ tests/apps-plink-1.90.150618.sh
@@ -1,1 +1,15 @@
-plink --help
+alces template prepare plink-1proc
+
+alces template copy plink-1proc ~/plink-1proc.sh
+
+/bin/bash -e -l ~/plink-1proc.sh
+
+echo
+echo
+echo
+
+alces template prepare plink-2proc
+
+alces template copy plink-2proc ~/plink-2proc.sh
+
+/bin/bash -e -l ~/plink-2proc.sh
