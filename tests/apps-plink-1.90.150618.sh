74c80101541eec78cc0dfff1a8cb1b29b7051327
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-10-05 15:13:20 +0100

    Updated Smoke test for plink v1.90.150618

--- tests/apps-plink-1.90.150618.sh
+++ tests/apps-plink-1.90.150618.sh
@@ -1,15 +1,13 @@
-alces template prepare plink-1proc
-
-alces template copy plink-1proc ~/plink-1proc.sh
-
-/bin/bash -e -l ~/plink-1proc.sh
-
-echo
-echo
-echo
-
-alces template prepare plink-2proc
-
-alces template copy plink-2proc ~/plink-2proc.sh
-
-/bin/bash -e -l ~/plink-2proc.sh
+if [ -z "$CW_DOCPATH" ]; then
+  plink --help
+else
+  alces template prepare plink-1proc
+  alces template copy plink-1proc ~/plink-1proc.sh
+  /bin/bash -e -l ~/plink-1proc.sh
+  echo
+  echo
+  echo
+  alces template prepare plink-2proc
+  alces template copy plink-2proc ~/plink-2proc.sh
+  /bin/bash -e -l ~/plink-2proc.sh
+fi
