e10568731c0788f5db6c62c9c13abf168f46a6a6
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-10-05 11:05:42 +0100

    Updated Smoke Test for IMB v4.0 to support with/without example script

--- tests/apps-imb-4.0.sh
+++ tests/apps-imb-4.0.sh
@@ -1,3 +1,6 @@
-alces template copy imb-4proc ~/imb.sh
-
-/bin/bash -e -l ~/imb.sh
+if [ -z "$CW_DOCPATH" ]; then
+  IMB-MPI1
+else
+  alces template copy imb-4proc ~/imb.sh
+  /bin/bash -e -l ~/imb.sh
+fi
