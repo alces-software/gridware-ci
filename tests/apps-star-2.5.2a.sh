4a60d582601968e0532e3b32bac055322c96aff1
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-10-05 15:24:40 +0100

    Updated Smoke Test for STAR v2.52a

--- tests/apps-star-2.5.2a.sh
+++ tests/apps-star-2.5.2a.sh
@@ -1,5 +1,7 @@
-alces template prepare star
-
-alces template copy star ~/star.sh
-
-/bin/bash -e -l ~/star.sh
+if [ -z "$CW_DOCPATH" ]; then
+  STAR
+else
+  alces template prepare star
+  alces template copy star ~/star.sh
+  /bin/bash -e -l ~/star.sh
+fi
