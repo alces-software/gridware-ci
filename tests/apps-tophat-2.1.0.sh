8fc4b53afcf8e68b536ad3c2abcb089dea149a69
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-10-05 15:23:01 +0100

    Updated Smoke Test for TopHat v2.1.0

--- tests/apps-tophat-2.1.0.sh
+++ tests/apps-tophat-2.1.0.sh
@@ -1,5 +1,7 @@
-alces template prepare tophat
-
-alces template copy tophat ~/tophat.sh
-
-/bin/bash -e -l ~/tophat.sh
+if [ -z "$CW_DOCPATH" ]; then
+  tophat --version
+else
+  alces template prepare tophat
+  alces template copy tophat ~/tophat.sh
+  /bin/bash -e -l ~/tophat.sh
+fi
