63bf06824a0236202fb51f4ba7aa1525fa21a4ed
Ruan G. Ellis <ruan.ellis@alces-software.com>
2016-10-05 15:17:30 +0100

    Updated Smoke Test for SOAPdenovo 2r240

--- tests/apps-soapdenovo-2r240.sh
+++ tests/apps-soapdenovo-2r240.sh
@@ -1,5 +1,7 @@
-alces template prepare soapdenovo
-
-alces template copy soapdenovo ~/soapdenovo.sh
-
-/bin/bash -e -l ~/soapdenovo.sh
+if [ -z "$CW_DOCPATH" ]; then
+  SOAPdenovo-127mer
+else
+  alces template prepare soapdenovo
+  alces template copy soapdenovo ~/soapdenovo.sh
+  /bin/bash -e -l ~/soapdenovo.sh
+fi
