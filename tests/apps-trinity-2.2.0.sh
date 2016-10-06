21a479ebb33053d7c749200127c5a91fe11dd11e
Mark Titorenko <mark.titorenko@alces-software.com>
2016-10-06 17:14:49 +0100

    Don't rely on empty $CW_DOCPATH for smoke tests.

--- tests/apps-trinity-2.2.0.sh
+++ tests/apps-trinity-2.2.0.sh
@@ -1,6 +1,6 @@
-if [ -z "$CW_DOCPATH" ]; then
-  Trinity --cite
-else
+if [[ ":$CW_DOCPATH" == *":$TRINITYDIR"* ]]; then
   alces template copy trinity ~/trinity.sh
   /bin/bash -e -l ~/trinity.sh
+else
+    Trinity --cite
 fi
