fca5579e80f32a923cfbdddafe91c776c32ea01f
Mark Titorenko <mark.titorenko@alces-software.com>
2016-10-06 18:11:57 +0100

    Build trinity with samtools 1.3.

--- tweaks/apps-trinity-2.2.0.sh
+++ tweaks/apps-trinity-2.2.0.sh
@@ -1,5 +1,5 @@
 # install dependencies
-deps=(apps-bowtie-1.1.0 apps-samtools-1.2)
+deps=(apps-bowtie-1.1.0 apps-samtools-1.3)
 for dep in "${deps[@]}"; do
     docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
     docker commit $(docker ps -alq) $img:build
