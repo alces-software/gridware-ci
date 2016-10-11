9f0dbc67450179c21eec97e8294794f6bdbbbda7
Andrew Provis <andrew.provis@alces-software.com>
2016-10-11 17:58:40 +0100

    Updated Smoke Test for MACS v2.1.0.20150731

--- tests/apps-macs-2.1.0.20150731.sh
+++ tests/apps-macs-2.1.0.20150731.sh
@@ -1,9 +1,11 @@
-if [ -z "$CW_DOCPATH" ]; then
-    :
+if [[ ":$CW_DOCPATH" == *":$MACSDIR"* ]]; then
+
+  alces template prepare macs
+
+  alces template copy macs ~/macs.sh
+
+  /bin/bash -e -l ~/macs.sh
+
 else
-    alces template prepare macs
-
-    alces template copy macs ~/macs.sh
-
-    /bin/bash -e -l ~/macs.sh
+  :
 fi
