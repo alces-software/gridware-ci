fbcaea01ac4ba68d01d27a535eeeb8da11fb682d
Andrew Provis <andrew.provis@alces-software.com>
2016-10-13 17:21:09 +0100

    Update Smoke Test for SOAPdenovo v2r240

--- tests/apps-soapdenovo-2r240.sh
+++ tests/apps-soapdenovo-2r240.sh
@@ -1,1 +1,11 @@
-SOAPdenovo-127mer --help
+if [[ ":$CW_DOCPATH" == *":$SOAPDENOVODIR"* ]]; then
+
+  alces template prepare soapdenovo
+
+  alces template copy soapdenovo ~/soapdenovo.sh
+
+  /bin/bash -e -l ~/soapdenovo.sh
+
+else
+  SOAPdenovo-127mer --help
+fi
