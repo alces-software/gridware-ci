f27b1129c78d5533a5b33e320ece5a1b84917a78
Andrew Provis <andrew.provis@alces-software.com>
2017-01-11 18:54:53 +0000

    Updated smoke test for MIRA v4.0.2

--- tests/apps-mira-4.0.2.sh
+++ tests/apps-mira-4.0.2.sh
@@ -1,1 +1,11 @@
-mira --version
+if [[ ":$CW_DOCPATH" == *":$MIRADIR"* ]]; then
+
+  alces template prepare mira
+
+  alces template copy mira ~/mira.sh
+
+  /bin/bash -e -l ~/mira.sh
+
+else
+  mira -version
+fi
