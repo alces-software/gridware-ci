528cd0af34dc1e88d89b26d0c4ec486cc413213b
Andrew Provis <andrew.provis@alces-software.com>
2017-01-30 14:53:13 +0000

    Updated smoke test for Libmhash v0.9.9.9

--- tests/libs-libmhash-0.9.9.9.sh
+++ tests/libs-libmhash-0.9.9.9.sh
@@ -1,1 +1,11 @@
-nm $LIBMHASHLIB/libmhash.so | grep mhash_keygen
+if [[ ":$CW_DOCPATH" == *":$LIBMHASHDIR"* ]]; then
+
+  alces template prepare libmhash
+
+  alces template copy libmhash ~/libmhash.sh
+
+  /bin/bash -e -l ~/libmhash.sh
+
+else
+  nm $LIBMHASHLIB/libmhash.so | grep mhash_keygen
+fi
