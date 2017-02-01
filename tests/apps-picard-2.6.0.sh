d844cdaa446dd3030c2e51f78b01c7678bfe733f
Andrew Provis <andrew.provis@alces-software.com>
2017-02-01 14:55:29 +0000

    Updated smoke test for Picard v2.6.0

--- tests/apps-picard-2.6.0.sh
+++ tests/apps-picard-2.6.0.sh
@@ -1,2 +1,13 @@
-wget https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24test/test1.bam
-picard AddCommentsToBam I=test1.bam O=output.bam C="comment"
+if [[ ":$CW_DOCPATH" == *":$PICARDDIR"* ]]; then
+
+  alces template prepare picard
+
+  alces template copy picard ~/picard.sh
+
+  /bin/bash -e -l ~/picard.sh
+
+else
+  wget https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24test/test1.bam
+
+  picard AddCommentsToBam I=test1.bam O=output.bam C="comment"
+fi
