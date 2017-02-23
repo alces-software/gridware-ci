c6fc7678f8c5655a9869761082f1fd8bc9ceed67
Ruan G. Ellis <ruan.ellis@alces-software.com>
2017-02-23 16:16:55 +0000

    Tweaks for Qiime v1.9.1

---
+++ tweaks/apps-qiime-1.9.1.sh
@@ -0,0 +1,9 @@
+install_args="uclust_licensing=understood"
+# install dependencies
+deps=(libs-atlas-3.10.2)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
