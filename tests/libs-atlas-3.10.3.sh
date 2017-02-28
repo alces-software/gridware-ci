36707abe17336ae02bf42ff05d80b30663cc31ab
Ruan G. Ellis <ruan.ellis@alces-software.com>
2017-02-28 15:05:31 +0000

    Smoke Test for ATLAS v3.10.3

---
+++ tests/libs-atlas-3.10.3.sh
@@ -0,0 +1,23 @@
+cat <<EOF > test.c
+#include <stdio.h>
+#include <cblas.h>
+
+int
+main ()
+{
+  int i;
+  double x[] = { 1, 1, 1 };
+
+
+  cblas_dscal(3, 4.323, x, 1);
+
+  for (i = 0; i < 3; ++i)
+    printf ("%f\n", x[i]);
+
+  return 0;
+}
+EOF
+
+gcc test.c -o testblas -L ${ATLASLIB} -lsatlas
+
+./testblas
