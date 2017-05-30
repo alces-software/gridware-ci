0935476be7aae1ab3d0a90497b6f3b7c722c01e5
Mark J. Titorenko <mark.titorenko@alces-software.com>
2017-05-30 15:39:55 +0100

    Build tweaks for Cluster Flow v0.6.0-20170530

---
+++ tweaks/apps-clusterflow-0.6.0-20170530.sh
@@ -0,0 +1,8 @@
+# install dependencies
+deps=(libs-atlas-3.10.3 libs-atlas-3.10.2)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
