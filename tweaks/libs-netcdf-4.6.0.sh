08169d2414bf00d47600452cf63f0a2e649a26c8
Mark J. Titorenko <mark.titorenko@alces-software.com>
2018-03-21 13:25:42 +0000

    Update deps for netcdf 4.6.0

--- tweaks/libs-netcdf-4.6.0.sh
+++ tweaks/libs-netcdf-4.6.0.sh
@@ -1,2 +1,10 @@
 install_args="--variant=all"
 export_packages="libs/netcdf/4.6.0 libs/netcdf_mpi/4.6.0"
+# install dependencies
+deps=(apps-hdf5_mpi-1.8.13)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
