e7968525666b0716ebcb7a9d393027eba3273deb
Mark J. Titorenko <mark.titorenko@alces-software.com>
2018-03-21 17:42:00 +0000

    Updated deps

--- tweaks/libs-netcdffortran-4.4.4.sh
+++ tweaks/libs-netcdffortran-4.4.4.sh
@@ -2,7 +2,7 @@ install_args="--variant=all"
 export_packages="libs/netcdffortran/4.4.4 libs/netcdffortran_mpi/4.4.4"
 export_args="--accept-bad 'lib/lib*.a'"
 # install dependencies
-deps=(apps-hdf5_mpi-1.8.13)
+deps=(apps-hdf5_mpi-1.8.13 apps-hdf5_serial-1.8.13)
 for dep in "${deps[@]}"; do
     docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
     docker commit $(docker ps -alq) $img:build
