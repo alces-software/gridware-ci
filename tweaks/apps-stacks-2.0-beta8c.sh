f5ea0c78bd1f7262107c0ced300574e20d516d1c
Mark J. Titorenko <mark.titorenko@alces-software.com>
2018-03-21 18:25:20 +0000

    Build tweaks for stacks 2.0-beta8c

---
+++ tweaks/apps-stacks-2.0-beta8c.sh
@@ -0,0 +1,8 @@
+install_args="--compiler gcc/4.9.0"
+deps=(compilers-gcc-4.9.0)
+for dep in "${deps[@]}"; do
+    docker run ${img}:build /bin/bash -c "curl -L https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/gridware/%24dist/${dep}-${cw_DIST}.tar.gz > /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+    docker run ${img}:build /bin/bash -l -c "alces gridware import /tmp/${dep}-${cw_DIST}.tar.gz"
+    docker commit $(docker ps -alq) $img:build
+done
